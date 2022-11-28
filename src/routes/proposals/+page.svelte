d7f0d4edcb76b79ae2464d29bb9ce97991461a88
hello-adam-martin <110892989+hello-adam-martin@users.noreply.github.com>
2022-11-28 16:16:51 +1300

    1 file modified

--- src/routes/proposals/+page.svelte
+++ src/routes/proposals/+page.svelte
@@ -17,16 +17,3 @@
 	<title>Proposals</title>
 	<meta name="description" content="Graphite Proposals" />
 </svelte:head>
-
-<div class="text-column">
-	<h1>Active Proposals</h1>
-
-	<p>
-		A unique & fair governance model that encourages participation by paying you to vote.
-	</p>
-	<p>
-		<b>Quadractic Voting</b><br>
-		Quadratic voting is a collective decision-making procedure which involves individuals allocating votes to express the degree of their preferences, rather than just the direction of their preferences.
-	</p>
-
-</div>
