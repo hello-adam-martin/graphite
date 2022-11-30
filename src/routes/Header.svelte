58218cc321322468b82414731948761b68e15230
hello-adam-martin <110892989+hello-adam-martin@users.noreply.github.com>
2022-11-30 14:21:52 +1300

    4 files modified

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,13 +1,13 @@
 <script>
 	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
+	import logo from '$lib/images/carblogo.png';
 	import github from '$lib/images/github.svg';
 </script>
 
 <header>
 	<div class="corner">
-		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
+		<a href="https://carbontoken.info">
+			<img src={logo} alt="Carbon Labs Logo" />
 		</a>
 	</div>
 
