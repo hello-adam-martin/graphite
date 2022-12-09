f70c1f1824661804c1a8494811f7960530cd8899
hello-adam-martin <110892989+hello-adam-martin@users.noreply.github.com>
2022-12-10 10:10:53 +1300

    2 files modified

--- src/routes/addproposal/+page.svelte
+++ src/routes/addproposal/+page.svelte
@@ -9,5 +9,7 @@
 
 <section>
 	<h1>Add A New Proposal</h1>
+	<h2>Do you have an idea for an improvement?</h2>
+	<p>Proposals need to be approved by a majority of the COT members before it goes live. Please be aware that there is no refund should the proposal not be approved. We suggest that you openly discuss your idea for a proposal with the community before starting a proposal.
   <AddProposal />
 </section>
